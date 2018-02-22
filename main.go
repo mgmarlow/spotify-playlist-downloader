28428089bf8dca7d9ef97842db57f26adfbeb010
Graham Marlow <mgmarlow@gmail.com>
2018-02-21 23:22:17 -0800

    implement client auth

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 type auth struct {
 	ClientID     string `json:"clientId"`
 	ClientSecret string `json:"clientSecret"`
+}
+
+type tokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
 }
 
 func main() {
@@ -21,7 +32,11 @@ func main() {
 
 	client := userAuth.ClientID + ":" + userAuth.ClientSecret
 	basicAuth := base64.StdEncoding.EncodeToString([]byte(client))
-	fmt.Printf("%s", basicAuth)
+	token, err := getToken(basicAuth)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", token.AccessToken)
 }
 
 func loadConfig(fileName string) (auth, error) {
@@ -39,4 +54,34 @@ func loadConfig(fileName string) (auth, error) {
 	}
 
 	return userAuth, nil
+}
+
+func getToken(basicAuth string) (tokenResponse, error) {
+	resp, err := requestToken(basicAuth)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var token tokenResponse
+	err = json.Unmarshal(body, &token)
+	return token, err
+}
+
+// Client auth strategy
+func requestToken(basicAuth string) (*http.Response, error) {
+	client := &http.Client{}
+	form := url.Values{
+		"grant_type": {"client_credentials"},
+	}
+	body := bytes.NewBufferString(form.Encode())
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Basic "+basicAuth)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	return client.Do(req)
 }
