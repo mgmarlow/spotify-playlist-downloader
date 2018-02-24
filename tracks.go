3dd2aac12f7fc7d7c52b8960e40867f776807720
Graham Marlow <mgmarlow@gmail.com>
2018-02-24 10:39:54 -0800

    refactor tracks

--- tracks.go
+++ tracks.go
@@ -95,33 +95,40 @@ type Tracks struct {
 // GetAllTracks returns all tracks from the provided playlist URI
 func GetAllTracks(playlistURI string, accessToken string) ([]Item, error) {
 	var trackItems []Item
+	queryParams := strings.Split(playlistURI, ":")
+	initialURI := "https://api.spotify.com/v1/users/" + queryParams[2] + "/playlists/" + queryParams[4] + "/tracks"
 
-	resp, err := requestPlaylistTracks(playlistURI, accessToken)
-	if err != nil {
-		return nil, err
-	}
-
-	initialTracks, err := getTracks(resp)
+	initialTracks, err := getTracks(initialURI, accessToken)
 	if err != nil {
 		return nil, err
 	}
 	trackItems = append(trackItems, initialTracks.Items...)
 
-	next := initialTracks.Next
-	for next != "" {
-		resp, err := requestPlaylistTracksFromFullPath(next, accessToken)
-		newTracks, err := getTracks(resp)
+	// Due to spotify's max return of 100 tracks, repeatedly request next page
+	nextURI := initialTracks.Next
+	for nextURI != "" {
+		newTracks, err := getTracks(nextURI, accessToken)
 		if err != nil {
 			return nil, err
 		}
+
 		trackItems = append(trackItems, newTracks.Items...)
-		next = newTracks.Next
+		nextURI = newTracks.Next
 	}
 
 	return trackItems, err
 }
 
-func getTracks(resp *http.Response) (Tracks, error) {
+func getTracks(uri string, accessToken string) (Tracks, error) {
+	resp, err := requestWithAuth(uri, accessToken)
+	if err != nil {
+		return Tracks{}, err
+	}
+
+	return unMarshallTracks(resp)
+}
+
+func unMarshallTracks(resp *http.Response) (Tracks, error) {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -132,21 +139,7 @@ func getTracks(resp *http.Response) (Tracks, error) {
 	return tracks, nil
 }
 
-func requestPlaylistTracks(playlistURI string, accessToken string) (*http.Response, error) {
-	queryParams := strings.Split(playlistURI, ":")
-	uri := "https://api.spotify.com/v1/users/" + queryParams[2] + "/playlists/" + queryParams[4] + "/tracks"
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", uri, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+accessToken)
-	return client.Do(req)
-}
-
-func requestPlaylistTracksFromFullPath(path string, accessToken string) (*http.Response, error) {
+func requestWithAuth(path string, accessToken string) (*http.Response, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", path, nil)
 	if err != nil {
